ceb5a057b8695e631fb2c21b31e8043bb9a02a5d
AdrianCamachoHerrera <0619592235@uma.es>
2019-12-07 17:32:13 +0100

    Fuente mas cercana y aparcamiento bici mas cercano a una posicion GPS añadidos.

--- src/java/service/DatosREST.java
+++ src/java/service/DatosREST.java
@@ -51,7 +51,7 @@ public class DatosREST {
     @GET
     @Path("aparcamientos_bici")
     @Produces(MediaType.APPLICATION_JSON)
-    public String aparcamientosREST(@PathParam("id") Integer id) throws MalformedURLException, IOException, JSONException {
+    public String aparcamientosREST() throws MalformedURLException, IOException, JSONException {
         installTrustManager();
         URL url = new URL("https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=3bb304f9-9de3-4bac-943e-7acce7e8e8f9");
         return getRecords(url).toString();
@@ -66,13 +66,31 @@ public class DatosREST {
         return getRecord("NUM_LIBRES", getRecords(url), id);
     }
     
-     @GET
+    @GET
     @Path("carriles_bici")
     @Produces(MediaType.APPLICATION_JSON)
-    public String carrilesBici(@PathParam("id") Integer id) throws MalformedURLException, IOException, JSONException {
+    public String carrilesBici() throws MalformedURLException, IOException, JSONException {
         installTrustManager();
         URL url = new URL("https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=9b3b8fe2-8c03-4420-b718-f36ea541f8fd");
         return getRecords(url).toString();
+    }
+    
+    @GET
+    @Path("fuente_cercana/{x}/{y}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String fuente_cercana(@PathParam("x") Double x, @PathParam("y") Double y ) throws MalformedURLException, IOException, JSONException {
+        installTrustManager();
+        URL url = new URL("https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=8d0f2d6e-b99d-42f1-929e-8bf25938af27");
+        return fuenteMasCercana(x,y,getRecords(url)).toString();
+    }
+    
+    @GET
+    @Path("bici_cercana/{x}/{y}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String bici_cercana(@PathParam("x") Double x, @PathParam("y") Double y ) throws MalformedURLException, IOException, JSONException {
+        installTrustManager();
+        URL url = new URL("https://datosabiertos.malaga.eu/api/3/action/datastore_search?resource_id=3bb304f9-9de3-4bac-943e-7acce7e8e8f9");
+        return biciMasCercana(x,y,getRecords(url)).toString();
     }
         
     
@@ -108,17 +126,6 @@ public class DatosREST {
         }
     }
 
-    private String getRecord( String record, JSONArray records, Integer id) throws JSONException {
-        int i=0;
-        while(i<records.length()){
-            if(records.getJSONObject(i).getInt("ID")==id){
-                return records.getJSONObject(i).get(record).toString();
-            }
-            i++;
-        }
-        return "-1";
-    }
-
     private JSONArray getRecords(URL url) throws JSONException, IOException {
         HttpsURLConnection conn = (HttpsURLConnection)url.openConnection();
         BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
@@ -133,6 +140,61 @@ public class DatosREST {
         JSONObject json = new JSONObject(response.toString());
         JSONArray records = json.getJSONObject("result").getJSONArray("records");
         return records;
+    }
+    
+    private String getRecord( String record, JSONArray records, Integer id) throws JSONException {
+        int i=0;
+        while(i<records.length()){
+            if(records.getJSONObject(i).getInt("ID")==id){
+                return records.getJSONObject(i).get(record).toString();
+            }
+            i++;
+        }
+        return "-1";
+    }
+
+    private JSONObject fuenteMasCercana(Double x, Double y, JSONArray records) throws JSONException {
+        int i=0;
+        JSONObject jo = null;
+        JSONObject res = null;
+        Double dist = Double.MAX_VALUE;
+        while(i<records.length()){
+            jo = records.optJSONObject(i);
+            if(distancia(jo.getString("wkb_geometry"), x, y)< dist){
+                dist = distancia(jo.getString("wkb_geometry"), x, y);
+                res = jo;
+            }
+            i++;
+        }
+        return res;
+    }
+    
+    private JSONObject biciMasCercana(Double x, Double y, JSONArray records) throws JSONException {
+        int i=0;
+        JSONObject jo = null;
+        JSONObject res = null;
+        Double dist = Double.MAX_VALUE;
+        while(i<records.length()){
+            jo = records.optJSONObject(i);
+            if(distancia(jo.getDouble("LAT"), jo.getDouble("LON"), x, y)< dist){
+                dist = distancia(jo.getDouble("LAT"), jo.getDouble("LON"), x, y);
+                res = jo;
+            }
+            i++;
+        }
+        return res;
+    }
+
+    private Double distancia(String punto, Double x, Double y) {
+        punto = punto.substring(7,punto.length()-1);
+        String[] xy = punto.split(" ");
+        Double xp = Double.parseDouble(xy[0]);
+        Double yp = Double.parseDouble(xy[1]);
+        return Math.sqrt(Math.pow(x-xp, 2) + Math.pow(y-yp, 2));
+    }
+
+    private Double distancia(double latitud, double longitud, Double x, Double y) {
+        return Math.sqrt(Math.pow(latitud-x, 2) + Math.pow(longitud-y, 2));
     }
 
 }
