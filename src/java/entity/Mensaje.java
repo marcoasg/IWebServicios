5e08cbc16b3a2292df7c91f11093da93061e3c50
marcoasg <trigitos@uma.es>
2019-12-12 22:17:12 +0100

    añadido auto increment

--- src/java/entity/Mensaje.java
+++ src/java/entity/Mensaje.java
@@ -10,6 +10,7 @@ import java.util.Date;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -45,6 +46,7 @@ public class Mensaje implements Serializable {
     @Column(name = "texto")
     private String texto;
     @Id
+    @GeneratedValue
     @Basic(optional = false)
     @NotNull
     @Column(name = "id")
